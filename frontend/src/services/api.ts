0317c8b2dce339f4146f7432cb92c6265122478d
Miguel-drm <mglderoma@gmail.com>
2025-06-19 21:28:35 +0800

    deploying

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import { db } from '../config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = `${import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : 'http://localhost:5000'}/api`;
 
 export interface Story {
   _id: string;
